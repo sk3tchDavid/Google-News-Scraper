4026cc49690d5c2cb2f6c581a3aaa330e9a0ab0a
David <66833306+sk3tchDavid@users.noreply.github.com>
2023-09-07 01:35:37 +0200

    basic functionality
    Gets the current main article from the Tagesschau and putts it in a TXT (with Current time and date)

--- NewsScraper.java
+++ NewsScraper.java
@@ -17,50 +17,50 @@ public class NewsScraper {
 
         System.out.println(a);
 
-        Write(a);
+        Write(a); //writes the news article in the txt
 
-        }
+    }
 
     public static String ScrapeTagesschau() {
-            String topic;
-            String head;
-            String content;
-            try {
-                String url = "https://www.tagesschau.de/";
+        String topic;
+        String head;
+        String content;
+        try {
+            String url = "https://www.tagesschau.de/";
 
-                // Connect to the URL
-                Document document = Jsoup.connect(url).get();
+            // Connect to the URL
+            Document document = Jsoup.connect(url).get();
 
-                // Locate news (different for every website so adjust)
-                Element Etopic = document.select("div.teasergroup:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > span:nth-child(1)").first();
-                Element Ehead = document.select("div.teasergroup:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > span:nth-child(2)").first();
-                Element Econtent = document.select("div.teasergroup:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)").first();
+            // Locate news (different for every website so adjust!!!)
+            Element Etopic = document.select("div.teasergroup:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > span:nth-child(1)").first();
+            Element Ehead = document.select("div.teasergroup:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(1) > span:nth-child(2)").first();
+            Element Econtent = document.select("div.teasergroup:nth-child(1) > div:nth-child(1) > div:nth-child(2) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1) > p:nth-child(2)").first();
 
-                // Extract and return the News
-                if (Etopic != null) {
-                    topic = Etopic.text();
-                    head = Ehead.text();
-                    content = Econtent.text();
-                    return "\nThema: " + topic + " - " + head + "\nInhalt: " + content;
-                } else {
-                    return "ERROR: news not found";
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            // Extract and return the News
+            if (Etopic != null) {
+                topic = Etopic.text();
+                head = Ehead.text();
+                content = Econtent.text();
+                return "\nThema: " + topic + " - " + head + "\nInhalt: " + content;
+            } else {
+                return "ERROR: news not found";
             }
-
-            return "ERROR: Exception_Scrape";
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        public static String Write(String a) throws IOException {
-            try {
-                FileWriter writer = new FileWriter("news.txt", true);
-                writer.write(a);
-                writer.close();
-                return "success";
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return "ERROR: Exception_Write";
+        return "ERROR: Exception_Scrape";
+    }
+
+    public static String Write(String a) throws IOException {
+        try {
+            FileWriter writer = new FileWriter("news.txt", true);
+            writer.write(a);
+            writer.close();
+            return "success";
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return "ERROR: Exception_Write";
+    }
 }
